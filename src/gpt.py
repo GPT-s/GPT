a31c51382c2d28f31fbbb56b1eddf55b2d3ce6e0
정현호 <gusgh0217@gmail.com>
2023-04-13 20:40:59 +0900

    크롤링, gpt요약 모듈화

--- src/gpt.py
+++ src/gpt.py
@@ -1,1 +1,31 @@
 # gpt 
+import openai
+
+OPENAI_API_KEY = "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"
+openai.api_key = OPENAI_API_KEY
+
+def summarize(text):
+    
+    model_engine = "text-davinci-002"
+
+    max_tokens = 2500
+    
+    if text is not None:
+        query = f'''Summarize the following paragraphs.
+
+        {text}
+        ''' 
+    else:
+        print("empty text.")
+
+
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=query,
+        max_tokens=max_tokens,
+        temperature=0.3,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return completion.choices[0].text
