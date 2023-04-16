e7fc6d8d1bb36b7eb61b0f6aa6f005cc7a3bcf0f
장향미 <hhh111gidal@naver.com>
2023-04-16 18:30:50 +0900

    지수크롤링진짜

--- src/stock_idx.py
+++ src/stock_idx.py
@@ -1,23 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import random
+import time
 from fake_useragent import UserAgent
 
-def get_stock_info(stock_symbol):
-    url = f"https://finance.yahoo.com/quote/{stock_symbol}"
-    headers = {"User-Agent": UserAgent().random}
-    try:
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.content, "html.parser")
-        price = soup.select_one('#quote-header-info > div > div > div > span[data-reactid="32"]').text
-        change = soup.select_one('#quote-header-info > div > div > div > span[data-reactid="50"]').text
-        print(f"Price: {price}")
-        print(f"Change: {change}")
-    except:
-        print("Error: Request failed")
+def get_stock_info(stock_code, num_updates):
+    user_agent = UserAgent()
 
+    url = "https://finance.yahoo.com"
+    search_query = "/quote/"
+    
+    for i in range(num_updates):
+        try:
+            headers = {"User-Agent": user_agent.random}
+            
+            response = requests.get(url + search_query + stock_code, headers=headers)
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-import stock_idx
+            price_element = soup.select_one("#quote-header-info > div.My\(6px\).Pos\(r\).smartphone_Mt\(6px\).W\(100\%\) > div.D\(ib\).Va\(m\).Maw\(65\%\).Ov\(h\) > div.D\(ib\).Mend\(20px\) > fin-streamer:nth-child(3) > span")
+            price = price_element.text if price_element is not None else "N/A"
 
-yahoo_finance.get_stock_info("AAPL")  
+            change_element = soup.select_one("#quote-header-info > div.My\(6px\).Pos\(r\).smartphone_Mt\(6px\).W\(100\%\) > div.D\(ib\).Va\(m\).Maw\(65\%\).Ov\(h\) > div.Fz\(12px\).C\(\$tertiaryColor\).My\(0px\).D\(ib\).Va\(b\) > span.Fw\(500\) > fin-streamer:nth-child(2) > span")
+            change = change_element.text if change_element is not None else "N/A"
 
-# AAPL에 원하는 종목 이름 
+            print(f"{stock_code}: {price} ({change})")
+
+        except:
+            print(f"Error: Failed to get stock info for {stock_code}")
+
+        time.sleep0)
