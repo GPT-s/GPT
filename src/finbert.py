d495c41d5e933c7f430f7374874c9b4e8f380818
장예지 <jyj0993@naver.com>
2023-04-17 16:32:28 +0900

    장예지 커밋

--- src/finbert.py
+++ src/finbert.py
@@ -1,47 +1,23 @@
-
-
-
-
-
- #!git clone https://gist.github.com/c1a8c0359fbde2f6dcb92065b8ffc5e3.git
-
-#import pandas
-
-
-
-#headlines_df = pandas.read_csv('c1a8c0359fbde2f6dcb92065b8ffc5e3/300_stock_headlines.csv')
-#headlines_df.head(5)
-
-#import numpy as np
-
-#headlines_array = np.array(headlines_df)
-#np.random.shuffle(headlines_array)
-#headlines_list = list(headlines_array[:,2])
-
-
-#print(headlines_list)
-
-from crawler import top5_text
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import pandas as pd
 import torch
 
 
 # finbert 시작
-# data =crawler사용
+# data =crawler(top5_text) 사용
 def bert(data):
     # 모델 설정
     tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
     model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
   
     inputs = tokenizer(data, padding = True, truncation = True, return_tensors='pt')
-    print(inputs)
+    #print(inputs)
     
     outputs = model(**inputs)
-    print(outputs.logits.shape)
+    #print(outputs.logits.shape)
 
     predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)   
-    print(predictions)
+    #print(predictions)
 
     positive = predictions[:, 0].tolist()
     negative = predictions[:, 1].tolist()
@@ -51,10 +27,11 @@ def bert(data):
              "Positive":positive,
              "Negative":negative, 
              "Neutral":neutral}
-      
-    df = pd.DataFrame(table, columns = ["data", "Positive", "Negative", "Neutral"])
+
+
+    sentiment_data = pd.DataFrame(table, columns = ["data", "Positive", "Negative", "Neutral"])
     
-    return df
+    return sentiment_data
 
 
 
