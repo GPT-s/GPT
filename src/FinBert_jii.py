b61f11a025cc729919b986ec2558a0c9a81009b0
장예지 <jyj0993@naver.com>
2023-04-17 11:37:32 +0900

    finbert-jii완료

--- src/FinBert_jii.py
+++ src/FinBert_jii.py
@@ -3,100 +3,58 @@
 
 
 
-# 코랩을 시작할 때 아래코드를 한 번 돌려줍니다.
-!pip install selenium
-!apt-get update
-!apt install chromium-chromedriver
-!cp /usr/lib/chromium-browser/chromedriver /usr/bin
+ #!git clone https://gist.github.com/c1a8c0359fbde2f6dcb92065b8ffc5e3.git
+
+#import pandas
 
 
 
+#headlines_df = pandas.read_csv('c1a8c0359fbde2f6dcb92065b8ffc5e3/300_stock_headlines.csv')
+#headlines_df.head(5)
 
-!git clone https://gist.github.com/c1a8c0359fbde2f6dcb92065b8ffc5e3.git
+#import numpy as np
 
-
-
-
-
-
-"""# 새 섹션"""
-
+#headlines_array = np.array(headlines_df)
+#np.random.shuffle(headlines_array)
+#headlines_list = list(headlines_array[:,2])
 
 
+#print(headlines_list)
 
-
-
-
-
-
-
-
-import pandas
-
-
-headlines_df = pandas.read_csv('c1a8c0359fbde2f6dcb92065b8ffc5e3/300_stock_headlines.csv')
-headlines_df.head(5)
-
-import numpy as np
-
-
-headlines_array = np.array(headlines_df)
-np.random.shuffle(headlines_array)
-headlines_list = list(headlines_array[:,2])
-
-
-print(headlines_list)
-
-
-
+from crawler import top5_text
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-  
-tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
-model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
-
-inputs = tokenizer(headlines_list, padding = True, truncation = True, return_tensors='pt')
-print(inputs)
-
-outputs = model(**inputs)
-print(outputs.logits.shape)
-
+import pandas as pd
 import torch
 
 
-predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
-print(predictions)
+# finbert 시작
+# data =crawler사용
+def bert(data):
+    # 모델 설정
+    tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
+    model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
+  
+    inputs = tokenizer(data, padding = True, truncation = True, return_tensors='pt')
+    print(inputs)
+    
+    outputs = model(**inputs)
+    print(outputs.logits.shape)
 
-import pandas as pd
+    predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)   
+    print(predictions)
 
+    positive = predictions[:, 0].tolist()
+    negative = predictions[:, 1].tolist()
+    neutral = predictions[:, 2].tolist()
 
-positive = predictions[:, 0].tolist()
-negative = predictions[:, 1].tolist()
-neutral = predictions[:, 2].tolist()
-
-
-table = {'Headline':headlines_list,
-         "Positive":positive,
-         "Negative":negative, 
-         "Neutral":neutral}
+    table = {'data':data,
+             "Positive":positive,
+             "Negative":negative, 
+             "Neutral":neutral}
       
-df = pd.DataFrame(table, columns = ["Headline", "Positive", "Negative", "Neutral"])
-
-
-df.head(5)
-
-!pip install wandb
-import wandb
-
-wandb.init(project="FinBERT_Sentiment_Analysis_Project")
-
-wandb.run.log({"Financial Sentiment Analysis Table" : wandb.Table(dataframe=df)})
-wandb.run.finish()
-
-cd /content/drive/MyDrive/Colab Notebooks/Git
-
-!git clone https://[jyj0993@naver.com]:[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]@github.com/[jyj0993@naver.com]/[GPT].git
-
-
+    df = pd.DataFrame(table, columns = ["data", "Positive", "Negative", "Neutral"])
+    
+    return df
 
 
 
