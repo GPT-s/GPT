6e76ae577bdb16ed25064785d5166f807673959e
권선택 <125417038+saintlucias@users.noreply.github.com>
2023-04-18 15:13:37 +0900

    main.py

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ import src.stock_idx
 import src.telegram_handler
 
 
-text = src.crawler.investing_news()
-print(text)
+#text = src.crawler.investing_news()
+#print(text)
+src.telegram_handler.main()
