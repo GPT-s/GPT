94d15dc45ff886a6b34155811b8667027cf24a6c
권선택 <saintlucias@naver.com>
2023-04-19 01:35:18 +0900

    req

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import src.crawler
+#import src.crawler
 import src.finbert
 import src.gpt
 import src.gpt_sent
 import src.stock_idx
 import src.telegram_handler as th
-
+import src.test_crawler
 
 #text = src.crawler.investing_news()
 #print(text)
-th.main()
+#th.main()
+src.test_crawler.Crawler()
