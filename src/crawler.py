b92c604f1a6a3e9e1ecedafe3c7c7f6475114b9e
정현호 <gusgh0217@gmail.com>
2023-04-18 14:18:20 +0900

    검색

--- src/crawler.py
+++ src/crawler.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -42,12 +43,42 @@ def yahoo_crawl_page(url):
     return text
 
 
-# 여러 링크를 한번에 크롤링하는 함수 작동원리는 공부 좀 해야할듯
+# 여러 링크를 한번에 띄우고 크롤링하는 함수 작동원리는 공부 좀 해야할 듯
 def crawl_links(links, crawl_func):
     with ThreadPoolExecutor() as executor:
         futures = [executor.submit(crawl_func, link) for link in links]
         results = [future.result() for future in as_completed(futures)]
     return results
+
+
+
+
+
+# 인베스팅 종목 검색해서 뉴스 링크 가져오고 크롤링해서 출력
+def investing_search():
+    investing_latest = set_chrome_driver(False)
+    investing_latest.get('https://www.investing.com/')
+    search = investing_latest.find_element(By.CSS_SELECTOR, '.js-main-search-bar')
+    search.send_keys('tsla')
+    search.send_keys(Keys.ENTER)
+    div_name = investing_latest.find_element(By.CSS_SELECTOR, '.js-inner-all-results-quotes-wrapper')
+    a_name = div_name.find_element(By.CSS_SELECTOR, 'a')
+    a_name.click()
+    a_name2 = investing_latest.find_element(By.CSS_SELECTOR, 'a[data-test="link-news"]')
+    a_name2.click()
+
+    investing_latest_links = []
+
+    for link in investing_latest.find_element(By.CLASS_NAME, 'mediumTitle1').find_elements(By.CLASS_NAME, 'js-article-item')[:3]:
+        investing_latest_links.append(link.find_element(By.CSS_SELECTOR, 'a').get_attribute('href'))
+    investing_latest.quit()
+
+    investing_text = crawl_links(investing_latest_links, investing_crawl_page)
+
+    print("인베스팅 검색 후 크롤링")
+    for text in investing_text:
+        print(text)
+        print('─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────')
 
 
 # 인베스팅 뉴스 링크 가져오고 크롤링해서 출력
@@ -87,4 +118,8 @@ def crawler_main():
     investing_news()
     yahoo_news()
 
-crawler_main()
+# crawler_main()
+
+# investing_search()
+
+# investing_news()
