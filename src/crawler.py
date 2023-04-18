c0fb79c5dcd5be60eae47af474770b9d88c26483
정현호 <gusgh0217@gmail.com>
2023-04-18 10:48:07 +0900

    모듈화

--- src/crawler.py
+++ src/crawler.py
@@ -49,28 +49,42 @@ def crawl_links(links, crawl_func):
     return results
 
 
-# 링크 가져오는 거
-def investing_links():
-    driver = set_chrome_driver(False)
-    driver.get('https://www.investing.com/news/most-popular-news')
-    top3_links = []
-    for link in driver.find_element(By.CLASS_NAME, 'largeTitle').find_elements(By.CLASS_NAME, 'js-article-item')[:3]:
-        top3_links.append(link.find_element(By.CSS_SELECTOR, 'a').get_attribute('href'))
-    driver.quit()
-    return top3_links
+# 인베스팅 뉴스 링크 가져오고 크롤링해서 출력
+def investing_news():
+    investing_top3 = set_chrome_driver(False)
+    investing_top3.get('https://www.investing.com/news/most-popular-news')
+    investing_top3_links = []
+    for link in investing_top3.find_element(By.CLASS_NAME, 'largeTitle').find_elements(By.CLASS_NAME, 'js-article-item')[:3]:
+        investing_top3_links.append(link.find_element(By.CSS_SELECTOR, 'a').get_attribute('href'))
+    investing_top3.quit()
 
-# 출력
-def print_news(links, crawl_func):
-    news_text = crawl_links(links, crawl_func)
-    print('─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────')
+    investing_top3_text = crawl_links(investing_top3_links, investing_crawl_page)
+
     print("인베스팅 Top3")
-    for text in news_text:
+    for text in investing_top3_text:
         print(text)
         print('─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────')
 
-# 링크 가져온 거랑 출력하는 거 한 번에 실행
-def crawler_main():
-    links = investing_links()
-    print_news(links, investing_crawl_page)
 
-crawler_main()
+
+# 야후 파이낸스 뉴스 링크 가져오고 크롤링해서 출력
+def yahoo_news():
+    yahoo_latest = set_chrome_driver(False)
+    yahoo_latest.get('https://finance.yahoo.com/news/')
+    yahoo_latest_links = []
+    for link in yahoo_latest.find_element(By.ID, 'Fin-Stream-Proxy').find_elements(By.CLASS_NAME, 'js-stream-content')[:3]:
+        yahoo_latest_links.append(link.find_element(By.CSS_SELECTOR, 'a').get_attribute('href'))
+    yahoo_latest.quit()
+    
+    yahoo_latest_text = crawl_links(yahoo_latest_links, yahoo_crawl_page)
+
+    print("야후 파이낸스 최신뉴스")
+    for text in yahoo_latest_text:
+        print(text)
+        print('─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────')
+
+
+if __name__ == '__main__':
+    investing_news()
+    yahoo_news()
+
