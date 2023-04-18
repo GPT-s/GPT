3e30d4c762948c342a44866d104fdaf345f834e3
권선택 <125417038+saintlucias@users.noreply.github.com>
2023-04-18 09:32:47 +0900

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import src.crawler
+from src.crawler import *
 
 links = ['https://www.investing.com/news/most-popular-news']
-text = src.investing_crawl_page(links)
+text = investing_crawl_page(links)
 print(text)
