5a9e9d013d4e09ed179b7fb74cc79899b32b7f5b
권선택 <125417038+saintlucias@users.noreply.github.com>
2023-04-19 15:09:23 +0900

    main 주석

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 import src.crawler  
 from src.finbert import bert 
-from src.gpt import get_completion, summarize, get_summary_list
-from src.gpt_sent import sentiment_analysis
-import src.stock_idx  
-from src.telegram_handler import start, help, send_message, message_text, favorites, message_handler, remove_favorite, on_callback_query, main
+from src.gpt import summarize
+#import src.gpt_sent
+#import src.stock_idx  
+from src.telegram_handler import send_message
 from src.crawler import investing_latest_news
 
 investing_latest_news = src.crawler.investing_latest_news()
 
-
-#print(investing_search)
-
-for text in investing_latest_news:
+for text in investing_latest_news: # 모듈 통합 실행 코드 
     finbert = bert
-    result_bert = finbert(text) # 핀버트 감성 분석 변수 선언
+    result_bert = finbert(text) # 핀버트 감성 분석 변수
     gpt = summarize
-    result_gpt = summarize(text) # 지피티 요약 변수 선언
+    result_gpt = summarize(text) # 지피티 요약 변수
     print('─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────')
     print(result_gpt)
-    telegram_send_message = send_message("5999372705", text =f"{result_gpt}\n{text}\n{result_bert}")
+    telegram_send_message = send_message("5999372705", text =f"{result_gpt}\n{text}\n{result_bert}") # 해당 아이디의 텔레그램에 내용 전송
     telegram_send_message
-
-
-
-
