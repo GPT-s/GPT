a0e379b267d4e98e820832d363c4798c21a7a7bc
정현호 <gusgh0217@gmail.com>
2023-04-21 11:27:30 +0900

    ,

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.gpt import summarize
 #import src.gpt_sent
 #import src.stock_idx  
 from src.telegram_handler import TelegramHandler
-from src.crawler import InvestingCrawler
+from src.crawler import InvestingCrawler, translator_deepl
 from src.gpt_keyword import get_analyze_keyword, analyze_keyword
 
 
@@ -16,6 +16,6 @@ for text in investing_latest_news: # 모듈 통합 실행 코드
     result_sentiment = finbert.sentiment(text)# 핀버트 감성 분석 변수
     result_gpt = summarize(text) # 지피티 요약 변수
     print('─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────')
-    translator = deepL_Translator(result_gpt)
+    translator = translator_deepl(result_gpt)
     telegram_send_message = send_message.send_message("5999372705", text =f"**요약된 뉴스**\n{translator}\n\n'**감성 분석**'\n\n{result_sentiment}\n\n**뉴스 링크**\n\n{text}") # 해당 아이디의 텔레그램에 내용 전송
     telegram_send_message
