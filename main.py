ef4620019d23ca6b65642dd035bc184c46277ac7
장향미 <hhh111gidal@naver.com>
2023-04-21 11:47:57 +0900

    .

--- main.py
+++ main.py
@@ -18,3 +18,5 @@ for text in investing_latest_news: # 모듈 통합 실행 코드
     translator = deepL_Translator(result_gpt)
     telegram_send_message = send_message("5999372705", text =f"**요약된 뉴스**\n{translator}\n\n'**감성 분석**'\n\n{result_sentiment}\n\n**뉴스 링크**\n\n{text}") # 해당 아이디의 텔레그램에 내용 전송
     telegram_send_message
+
+    
