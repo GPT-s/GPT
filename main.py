3efa12cc0b73114ecc4fa821f04b7eb65407f9dc
권선택 <125417038+saintlucias@users.noreply.github.com>
2023-04-21 11:24:28 +0900

    main

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ from src.finbert import FinBert
 from src.gpt import summarize
 #import src.gpt_sent
 #import src.stock_idx  
-from src.telegram_handler import telegram_handler
-from src.crawler import investing_latest_news, deepL_Translator
+from src.telegram_handler import TelegramHandler
+from src.crawler import InvestingCrawler
 from src.gpt_keyword import get_analyze_keyword, analyze_keyword
 
+
 investing_latest_news = InvestingCrawler()
-Finbert = FinBert()
-send_message = telegram_handler()
+finbert = FinBert()
+send_message = TelegramHandler()
 
 for text in investing_latest_news: # 모듈 통합 실행 코드 
-    result_sentiment = Finbert.sentiment(text)# 핀버트 감성 분석 변수
+    result_sentiment = finbert.sentiment(text)# 핀버트 감성 분석 변수
     result_gpt = summarize(text) # 지피티 요약 변수
     print('─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────')
     translator = deepL_Translator(result_gpt)
-    telegram_send_message = send_message("5999372705", text =f"**요약된 뉴스**\n{translator}\n\n'**감성 분석**'\n\n{result_sentiment}\n\n**뉴스 링크**\n\n{text}") # 해당 아이디의 텔레그램에 내용 전송
+    telegram_send_message = send_message.send_message("5999372705", text =f"**요약된 뉴스**\n{translator}\n\n'**감성 분석**'\n\n{result_sentiment}\n\n**뉴스 링크**\n\n{text}") # 해당 아이디의 텔레그램에 내용 전송
     telegram_send_message
