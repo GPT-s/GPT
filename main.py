b1385c500530c9de2974bf3d77cbd4e028c1ec44
정현호 <gusgh0217@gmail.com>
2023-04-26 15:21:02 +0900

    fix: test하려고 했는데 원본에서 고쳐버림

--- main.py
+++ main.py
@@ -116,51 +116,51 @@ class Pipeline():
                 message = f"**요약된 뉴스**\n{translated_text}\n\n원문 링크: {news[1]}"
 
                 # 유저아이디 전부 가져오기
-                chat_id_list = self.database.select_user_id()
+        #         chat_id_list = self.database.select_user_id()
                 
-                # 모든 유저에게 메시지 보내기
-                for chat_id in chat_id_list:
-                    telegram_chat_id = chat_id[0]
-                    self.telegram_handler.send_message(telegram_chat_id, text=message)
+        #         # 모든 유저에게 메시지 보내기
+        #         for chat_id in chat_id_list:
+        #             telegram_chat_id = chat_id[0]
+        #             self.telegram_handler.send_message(telegram_chat_id, text=message)
 
-                self.database.update_news_sent(news[0])
+        #         self.database.update_news_sent(news[0])
 
-        print("작업 완")
-        end_time = time.time()
-        Total_time = end_time - start_time
+        # print("작업 완")
+        # end_time = time.time()
+        # Total_time = end_time - start_time
 
-        print(f"저장된 뉴스 가져와서 메시지 보내기까지 걸린 시간: {Total_time:.2f} 초")
+        # print(f"저장된 뉴스 가져와서 메시지 보내기까지 걸린 시간: {Total_time:.2f} 초")
                 
                 # 본인한테만 메시지 보내기
                 # 유저아이디 전부 가져오기 부터
                 # 저장된 뉴스 시간 프린트까지 주석 처리하고
                 # # 여기는 유저 아이디
-                # telegram_chat_id = '5292915370'
+                telegram_chat_id = '5292915370'
 
-                # # 텔레그램 메시지 전송을 변수로 만들어줌
-                # # 굳이 안만들어줘도 됨. 
-                # # 나중에 다른 곳에서 쓸 수도 있으니까 혹시 몰라서
-                # telegram_send_message = self.telegram_handler.send_message(telegram_chat_id, text=message)
+                # 텔레그램 메시지 전송을 변수로 만들어줌
+                # 굳이 안만들어줘도 됨. 
+                # 나중에 다른 곳에서 쓸 수도 있으니까 혹시 몰라서
+                telegram_send_message = self.telegram_handler.send_message(telegram_chat_id, text=message)
 
-                # # 해당 아이디의 텔레그램에 내용 전송 실행
-                # telegram_send_message
+                # 해당 아이디의 텔레그램에 내용 전송 실행
+                telegram_send_message
 
-                # # 뉴스를 전송한 후에 데이터베이스에 전송 여부를 업데이트합니다.
-                # self.database.update_news_sent(news[0])  # news[0]는 DB 테이블 1번째에 있는 컬럼
+                # 뉴스를 전송한 후에 데이터베이스에 전송 여부를 업데이트합니다.
+                self.database.update_news_sent(news[0])  # news[0]는 DB 테이블 1번째에 있는 컬럼
 
-        # print("작업 완")
-        # # 시간 측정
-        # end_time = time.time()
-        # Total_time = end_time - start_time
+        print("작업 완")
+        # 시간 측정
+        end_time = time.time()
+        Total_time = end_time - start_time
 
-        # print(f"저장된 뉴스 가져와서 메시지 보내기까지 걸린 시간: {Total_time:.2f} 초")
+        print(f"저장된 뉴스 가져와서 메시지 보내기까지 걸린 시간: {Total_time:.2f} 초")
 
 pipeline = Pipeline()
 
 # 스케줄러 설정
 
 # 테스트 해보려면 여기 아래 시간을 바꾸세요. 지금 시간에 1~2분 더해서 그러면 크롤링하고 저장함
-schedule.every().day.at("10:50").do(lambda: pipeline.process_and_send_news())
+schedule.every().day.at("15:14").do(lambda: pipeline.process_and_send_news())
 
 # 크롤링하는 함수를 5분 마다 실행
 schedule.every(5).minutes.do(lambda: pipeline.process_and_send_news())
@@ -169,7 +169,7 @@ schedule.every(5).minutes.do(lambda: pipeline.process_and_send_news())
 schedule.every().day.at("08:30").do(lambda: pipeline.send_translated_news())
 
 # 테스트 해보려면 여기 아래 시간을 바꾸세요. 위에 크롤링 작동 시간에 2분 더해서 그러면 저장한거 메시지 보냄
-schedule.every().day.at("10:52").do(lambda: pipeline.send_translated_news())
+schedule.every().day.at("15:16").do(lambda: pipeline.send_translated_news())
 
 # 스케줄 실행
 while True:
