215df3b576e4ca7cb3af25d8c5fe4ada5185aa3c
이철민 <cjfals1016@gmail.com>
2023-04-29 15:02:23 +0900

    fix:gpt,handler

--- src/gpt.py
+++ src/gpt.py
@@ -2,7 +2,7 @@
 import openai
 from dotenv import load_dotenv
 import os
-import time
+import re
 
 load_dotenv()
 
@@ -12,52 +12,85 @@ OPENAI_API_KEY = GPTAPI
 openai.api_key = OPENAI_API_KEY
 
 
-def get_completion(text, model="gpt-3.5-turbo"):
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": text},
-    ]
-    max_retries = 3
-    retries = 0
-    while retries < max_retries:
-        try:
-            response = openai.ChatCompletion.create(model=model, messages=messages)
-            answer = response["choices"][0]["message"]["content"]
-            return answer
-        except Exception as e:
-            print("Error:", e)
-            retries += 1
-            if retries == max_retries:
-                print("최대 재시도 횟수 초과")
-                return None
-            else:
-                print("재시도...")
-                time.sleep(1)
+# def get_completion(text, model="gpt-3.5-turbo"):
+#     messages = [
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": text},
+#     ]
+#     max_retries = 3
+#     retries = 0
+#     while retries < max_retries:
+#         try:
+#             response = openai.ChatCompletion.create(model=model, messages=messages)
+#             answer = response["choices"][0]["message"]["content"]
+#             return answer
+#         except Exception as e:
+#             print("Error:", e)
+#             retries += 1
+#             if retries == max_retries:
+#                 print("최대 재시도 횟수 초과")
+#                 return None
+#             else:
+#                 print("재시도...")
+#                 time.sleep(1)
+
+def keyword(text):
+    try:
+        gptquery = f"{text}in the sentence above Tell me keywords,\n\
+                                            Change keywords with stock codes to stock codes in Keywords and mark all keywords\n\
+                                            no explanation needed\n\
+                                            Example:\n\
+                                            Keywords: Amazon (AMZN), today's price, recent news.\n\
+                                            stock Keywords: [AMZN]or\n\
+                                            stock Keywords: [001122.ks]or\n\
+                                            no Keywords: [N/A]"  
+        messages = [
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": gptquery}
+                    ]
+        response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=messages
+                    )
+        answer = response['choices'][0]['message']['content']
+        stockcode = re.search(r'\[(.*?)\]', answer).group(1)
+        return stockcode
+    except Exception as e:
+        keyword()
+
+
 
 def summarize(text):
     print("gpt 1번 시작")
-    model_engine = "text-davinci-002"
-
-    max_tokens = 2500
-
-    if text is not None:
-        sentiment_list = ['positive', 'negative', 'neutral']
-        query = f"""Sentiment analysis the contents of [{text}], output one of three words {str(sentiment_list) } based on the analysis, summarize the contents of [] Korean in about 5 lines.
-        """
-    else:
-        print("empty text.")
-
-    completion = openai.Completion.create(
-        engine=model_engine,
-        prompt=query,
-        max_tokens=max_tokens,
-        temperature=0.3,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
-    print("gpt 1번 완")
-    return completion.choices[0].text
+    try:
+        gptquery = f"Sentimentally analyze [{text}] and select only one of Positive, Negative, or Negative"
+        messages = [
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": gptquery}
+                    ]
+        response = openai.ChatCompletion.create(
+                    temperature=0,
+                    top_p=0,
+                    model= "gpt-3.5-turbo",
+                    messages=messages
+                    )
+        sentiment_answer = response['choices'][0]['message']['content']
+        gptquery = f"Summarize [{text}] in 5 lines with a maximum of 38 characters per line."
+        messages = [
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": gptquery}
+                    ]
+        response = openai.ChatCompletion.create(
+                    temperature=0,
+                    top_p=0,
+                    model= "gpt-3.5-turbo",
+                    messages=messages
+                    )
+        summarize_answer = response['choices'][0]['message']['content']
+        print("gpt 1번 완")
+        return sentiment_answer,summarize_answer
+    except Exception as e:
+        summarize()
 
 
 def get_summary_list(text_list):
