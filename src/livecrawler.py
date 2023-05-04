0ca4399e220482905d4c0cfca5cf90e409025ce3
장향미 <hhh111gidal@naver.com>
2023-05-04 12:35:33 +0900

    fix:라이브크롤러 비동기동기

--- src/livecrawler.py
+++ src/livecrawler.py
@@ -17,16 +17,24 @@ import time
 
 
 
+
 os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
 
-class LiveCrawler:
+import asyncio
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+class AsyncLiveCrawler:
     def __init__(self):
         options = Options()
         options.headless = True
         self.driver = webdriver.Chrome(options=options)
     
-    # 인베스팅 종목 검색해서 뉴스 링크 가져오고 크롤링해서 출력
-    def set_chrome_driver(self, headless=True):
+    async def set_chrome_driver(self, headless=True):
         options = webdriver.ChromeOptions()
         if headless:
             options.add_argument("headless")
@@ -38,14 +46,12 @@ class LiveCrawler:
         )
         return driver
 
-
-    # 인베스팅 뉴스 기사 페이지에서 텍스트 가져오는 함수
-    def investing_crawl_page(self, url):
+    async def investing_crawl_page(self, url):
         max_retries = 5
         retries = 0
         while retries < max_retries:
             try:
-                driver = self.set_chrome_driver(False)
+                driver = await self.set_chrome_driver(True)
                 driver.get(url)
                 article_page = driver.find_element(By.CLASS_NAME, "articlePage")
                 text = article_page.text
@@ -55,22 +61,19 @@ class LiveCrawler:
                 print("Error:", e)
                 retries += 1
                 if retries == max_retries:
-                    print("최대 재시도 횟수 초과")
-                    gpt_error = "오류"
+                    print("최대 재시도 횟수 초과1")
+                    gpt_error = "오류1"
                     return gpt_error
 
-
-
-    # 인베스팅 종목 검색해서 뉴스 링크 가져오고 크롤링해서 출력
-    def investing_search(self,news):
+    async def investing_search(self, news):
         max_retries = 5
         retries = 0
         while retries < max_retries:
             try:
-                investing_stock_latest = self.set_chrome_driver(False)
+                investing_stock_latest = await self.set_chrome_driver(True)
                 investing_stock_latest.get("https://www.investing.com/")
                 search = investing_stock_latest.find_element(By.CSS_SELECTOR, ".js-main-search-bar")
-                search.send_keys(news)  # 텔레그램에서 받아와서 검색할 수 있는 지 알아봐야함
+                search.send_keys(news)  
                 search.send_keys(Keys.ENTER)
                 div_name = investing_stock_latest.find_element(
                     By.CSS_SELECTOR, ".js-inner-all-results-quotes-wrapper"
@@ -93,25 +96,20 @@ class LiveCrawler:
                 print("Error:", e)
                 retries += 1
                 if retries == max_retries:
-                    print("최대 재시도 횟수 초과")
-                    gpt_error = "오류"
+                    print("최대 재시도 횟수 초과2")
+                    gpt_error = "오류2"
                     return gpt_error
                 
                 
+# 메인에서 사용
+# async def main():
+#     st_time = time.time()
+#     crawler = AsyncLiveCrawler()
+#     links = await crawler.investing_search("AAPL")
+#     text = await crawler.investing_crawl_page(links)
+#     print(text)
+#     ed_time = time.time()
+#     to_time = ed_time - st_time
+#     print(f"걸린 시간 : {to_time:2f}초 ")
 
-def main():
-    start_time = time.time()  # 시작 시간 저장
-
-    crawler = LiveCrawler()
-    news_link = crawler.investing_search("Apple")
-    print(news_link)
-    news_text = crawler.investing_crawl_page(news_link)
-    print(news_text)
-
-    end_time = time.time()  # 종료 시간 저장
-    elapsed_time = end_time - start_time  # 실행 시간 계산
-    print(f"실행 시간: {elapsed_time:.2f}초")
-
-
-if __name__ == "__main__":
-    main()
+# asyncio.run(main())
