f35521aa62cd125324ad3009e189dd955093c854
장향미 <hhh111gidal@naver.com>
2023-05-04 12:40:04 +0900

    fix:라이브크롤러비동기동기

--- src/livecrawler.py
+++ src/livecrawler.py
@@ -12,23 +12,16 @@ import os
 import logging
 import pymysql
 import pymysql.err
-import asyncio
-import time
+
 
 
 
 
 os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
-
 import asyncio
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-class AsyncLiveCrawler:
+class LiveCrawler:
     def __init__(self):
         options = Options()
         options.headless = True
